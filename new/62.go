0f1ea20617f5469996c424f9e4c144f0bc76086d
yufe <yufe@example.com>
2026-09-10 10:56:03 +0800

    910

---
+++ new/62.go
@@ -0,0 +1,18 @@
+package new
+
+func uniquePaths(m int, n int) int {
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	dp[1][1] = 1
+	for i := 1; i < m; i++ {
+		for j := 1; j < n; j++ {
+			if i == 1 && j == 1 {
+				continue
+			}
+			dp[i][j] = dp[i-1][j] + dp[i][j-1]
+		}
+	}
+	return dp[m][n]
+}
